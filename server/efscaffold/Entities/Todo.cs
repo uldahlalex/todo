3e16c43247e09cc3e2d47a1b9dbd2658f191c3a5
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-07-30 11:16:30 +0200

    scaffolding

---
+++ server/efscaffold/Entities/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace efscaffold.Entities;
+
+public partial class Todo
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public decimal Priority { get; set; }
+
+    public bool Isdone { get; set; }
+}
